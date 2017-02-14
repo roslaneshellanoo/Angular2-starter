ac0b3902189ac9845ac0854d313ea2cfe7e93682
roslanelyashiv <roslane@shellanoo.com>
2017-02-15 00:11:24 +0200

    first commit! added http

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../shared/api.service'
 
 @Component({
   selector: 'my-about',
@@ -7,12 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() {
+  constructor(
+    public apiService: ApiService
+  ) {
     // Do stuff
+  }
+
+  public users;
+  // username = {};
+
+
+  loadUser() {
+    this.apiService.getUser().subscribe(
+      data => this.users = data
+    );
   }
 
   ngOnInit() {
     console.log('Hello About');
+
+
   }
 
 }
--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,6 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Injectable()
 export class ApiService {
   title = 'Angular 2';
+
+  constructor (
+    public http: Http
+  ) {}
+
+  getUser() {
+    return this.http.get('https://jsonplaceholder.typicode.com/users')
+      .map( (res:Response) => res.json());
+       // .map(response => response.json().data);
+
+  }
 }
